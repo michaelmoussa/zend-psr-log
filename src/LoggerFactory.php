03394ee7cb06f22db080a47a141e155f049ee4cb
Michael Moussa <michael.moussa@gmail.com>
2014-10-16 14:14:18 -0400

    Created LoggerFactory for the ZendPsrLog Logger
    It should behave the same as Zend's LoggerServiceFactory, only
    return a ZendPsrLog\Logger instance instead.

---
+++ src/LoggerFactory.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Zend PSR Log
+ *
+ * @link      http://github.com/michaelmoussa/zend-psr-log
+ * @license   http://opensource.org/licenses/MIT The MIT License
+ */
+namespace ZendPsrLog;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+/**
+ * Factory for creating ZendPsrLog logger instances.
+ */
+class LoggerFactory implements FactoryInterface
+{
+    /**
+     * Creates the ZendPsrLog logger.
+     *
+     * @param ServiceLocatorInterface $serviceLocator
+     * @return Logger
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $config = $serviceLocator->get('Config');
+        return new Logger(isset($config['log']) ? $config['log'] : []);
+    }
+}
---
+++ test/LoggerFactoryTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace ZendPsrLog;
+
+use Mockery as m;
+use Zend\Log\LoggerServiceFactory;
+use Zend\ServiceManager\ServiceManager;
+
+class LoggerFactoryTest extends \PHPUnit_Framework_TestCase
+{
+    public function testCreatesZendPsrLogger()
+    {
+        $serviceManager = new ServiceManager();
+        $serviceManager->setService('config', ['log' => ['writers' => [['name' => 'mock']]]]);
+
+        $factory = new LoggerFactory();
+        $this->assertInstanceOf('ZendPsrLog\Logger', $factory->createService($serviceManager));
+    }
+
+    /**
+     * The Zend\Log\LoggerServiceFactory can't be directly re-used because can't
+     * change the type of Logger it returns, so let's at least make sure that
+     * our LoggerFactory behaves the same way.
+     */
+    public function testCreatesLoggerEquivalentToHowTheZendLoggerServiceFactoryDoesIt()
+    {
+        $serviceManager = new ServiceManager();
+        $serviceManager->setService('config', ['log' => ['writers' => [['name' => 'mock']]]]);
+
+        $zendPsrLoggerFactory = new LoggerFactory();
+        $zendPsrLogger = $zendPsrLoggerFactory->createService($serviceManager);
+
+        $zendLoggerServiceFactory = new LoggerServiceFactory();
+        $zendLogger = $zendLoggerServiceFactory->createService($serviceManager);
+
+        $reflectedZendPsrLogger = new \ReflectionClass($zendPsrLogger);
+        $reflectedZendLogger = new \ReflectionClass($zendLogger);
+
+        /** @var \ReflectionProperty $property */
+        foreach ($reflectedZendLogger->getProperties() as $property) {
+            $this->assertEquals($property, $reflectedZendPsrLogger->getProperty($property->getName()));
+        }
+
+        $this->assertSame(
+            $reflectedZendLogger->getStaticProperties(),
+            $reflectedZendPsrLogger->getStaticProperties()
+        );
+    }
+}
