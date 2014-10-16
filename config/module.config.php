562d6bcfdee65f87ebe3939c4efca708430f43ff
Michael Moussa <michael.moussa@gmail.com>
2014-10-16 18:04:33 -0400

    Added the factory itself to the service config

--- config/module.config.php
+++ config/module.config.php
@@ -2,7 +2,10 @@
 return array(
     'service_manager' => array(
         'factories' => array(
-            'Zend\Log\Logger' => new \ZendPsrLog\LoggerFactory('Zend\Log\Logger')
+            'Zend\Log\Logger' => new \ZendPsrLog\LoggerFactory('Zend\Log\Logger'),
+            'ZendPsrLog\LoggerFactory' => function () {
+                return new \ZendPsrLog\LoggerFactory('Zend\Log\Logger');
+            }
         ),
     ),
 );
--- test/ModuleTest.php
+++ test/ModuleTest.php
@@ -36,6 +36,26 @@ class ModuleTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('ZendPsrLog\Logger', $serviceManager->get('Zend\Log\Logger'));
     }
 
+    public function testCanGetLoggerFactoryDependencyForDefaultZendLogger()
+    {
+        $application = \Zend\Mvc\Application::init(
+            array(
+                'modules' => array(
+                    'ZendPsrLog'
+                ),
+                'module_listener_options' => array(
+                    'module_paths' => array(
+                        __DIR__ . '/../src',
+                        __DIR__ . '/../vendor'
+                    ),
+                )
+            )
+        );
+        $serviceManager = $application->getServiceManager();
+
+        $this->assertInstanceOf('ZendPsrLog\LoggerFactory', $serviceManager->get('ZendPsrLog\LoggerFactory'));
+    }
+
     protected function setUp()
     {
         $this->module = new Module();
